5cbbda4e834ceaa6e220063ea1b96cc21279afac
Per Wahlström <per@diabol.se>
2016-09-23 16:18:58 +0200

    Request logging

--- src/main/java/se/diabol/gamify/resources/SlackResource.java
+++ src/main/java/se/diabol/gamify/resources/SlackResource.java
@@ -6,14 +6,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import se.diabol.gamify.api.SlackResponse;
 
 @Path("/gamify")
 @Produces(MediaType.APPLICATION_JSON)
 public class SlackResource {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SlackResource.class);
+	
 	@GET
 	public SlackResponse say(@QueryParam("user_name") String username, @QueryParam("text") String text) {
+		LOGGER.info("Request from {}: {}", username, text);
 		String result = "Congratulations @" + username + " on earning your " + text + " badge!";
 		
 		return new SlackResponse("in_channel", result);
